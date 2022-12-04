55016beb1edc3bfdb43d464faf56edfeb8950807
avcalitunahan <alitunahanavcu@gmail.com>
2022-12-04 22:07:22 +0300

    sudenin dosyaları ile update

--- markups-osteriax/assets/js/app.js
+++ markups-osteriax/assets/js/app.js
@@ -46,24 +46,25 @@ $(function() {
 				$(formMessages).text('Oops! An error occured and your message could not be sent.');
 			}
 		});
+		
+		//dropdown
+		$('.dropdown-menu a.dropdown-toggle').on('click', function(e) {
+			if (!$(this).next().hasClass('show')) {
+			  $(this).parents('.dropdown-menu').first().find('.show').removeClass("show");
+			}
+			var $subMenu = $(this).next(".dropdown-menu");
+			$subMenu.toggleClass('show');
+		  
+		  
+			$(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function(e) {
+			  $('.dropdown-submenu .show').removeClass("show");
+			});
+		  
+		  
+			return false;
+		  });
 
 	});
 
 });
-
-$('.dropdown-menu a.dropdown-toggle').on('click', function(e) {
-	if (!$(this).next().hasClass('show')) {
-	  $(this).parents('.dropdown-menu').first().find('.show').removeClass("show");
-	}
-	var $subMenu = $(this).next(".dropdown-menu");
-	$subMenu.toggleClass('show');
-  
-  
-	$(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function(e) {
-	  $('.dropdown-submenu .show').removeClass("show");
-	});
-  
-  
-	return false;
-  });
 
